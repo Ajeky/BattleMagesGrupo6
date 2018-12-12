e29c538a7223b58f67afd02e0bf65a51af68bfad
unknown <alediazuni@gmail.com>
2018-12-12 23:11:50 +0100

    Model ElementO

--- src/model/Elemento.java
+++ src/model/Elemento.java
@@ -1,7 +1,41 @@
 package model;
 
 public class Elemento {
-	
-	
 
+	private Habilidad[] habilidades;
+	private String nombreElemento;
+	
+	public Elemento() {
+	
+	}
+		
+	public Elemento(Habilidad[] habilidades, String nombreElemento) {
+		super();
+		this.habilidades = habilidades;
+		this.nombreElemento = nombreElemento;
+	}
+
+	public Habilidad[] getHabilidades() {
+		return habilidades;
+	}
+
+	public void setHabilidades(Habilidad[] habilidades) {
+		this.habilidades = habilidades;
+	}
+
+	public String getNombreElemento() {
+		return nombreElemento;
+	}
+
+	public void setNombreElemento(String nombreElemento) {
+		this.nombreElemento = nombreElemento;
+	}
+
+	@Override
+	public String toString() {
+		return "Elemento [Elemento = " + nombreElemento + "Habilidades = " + habilidades+"]";
+	}
+	
+	
+	
 }
