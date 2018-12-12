725075f25d70a29b962a3728eb86bac5bf780229
Daniel Troncoso <milenio204@hotmail.com>
2018-12-12 11:56:11 +0100

    Habilidad class created , Daniel Troncoso

--- src/model/Habilidad.java
+++ src/model/Habilidad.java
@@ -2,4 +2,48 @@ package model;
 
 public class Habilidad {
 
+	private String nombre;
+	int potencia;
+	int costeMana;
+
+	public Habilidad(String nombre, int potencia, int costeMana) {
+		super();
+		this.nombre = nombre;
+		this.potencia = potencia;
+		this.costeMana = costeMana;
+	}
+
+	public Habilidad() {
+
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getPotencia() {
+		return potencia;
+	}
+
+	public void setPotencia(int potencia) {
+		this.potencia = potencia;
+	}
+
+	public int getCosteMana() {
+		return costeMana;
+	}
+
+	public void setCosteMana(int costeMana) {
+		this.costeMana = costeMana;
+	}
+
+	@Override
+	public String toString() {
+		return "Habilidad [nombre=" + nombre + ", potencia=" + potencia + ", costeMana=" + costeMana + "]";
+	}
+
 }
