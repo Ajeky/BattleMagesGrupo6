16a98a5dcc5d8e1354ee48a97863ae5dc6e0ca4e
Dani Troncoso <milenio204@hotmail.com>
2018-12-13 10:44:23 +0100

    Array habilidades añadido a DatosHabilidad

--- src/datos/DatosHabilidad.java
+++ src/datos/DatosHabilidad.java
@@ -1,5 +1,13 @@
 package datos;
 
+import model.Habilidad;
+
 public class DatosHabilidad {
+	
+	private int tamanyo = 12;
+	private Habilidad[] listaHabilidades = {new Habilidad("Disparo de Fuego", 55, 20), new Habilidad("Llamarada Ardiente", 85, 45), new Habilidad("Barrera napalm", 0, 30), new Habilidad("Lanzar roca", 55, 20), 
+			new Habilidad("Avalancha rocosa", 85, 45), new Habilidad("Muro rocoso", 0, 30), new Habilidad("Escarchada selecta", 55, 20), new Habilidad("Bola nevada", 85, 45), new Habilidad("Iceberg", 0, 30), 
+			new Habilidad("Rayo francotirador", 55, 20), new Habilidad("Alterar protones", 85, 45), new Habilidad("Campo electroestático", 0, 30)};
+	
 
 }
